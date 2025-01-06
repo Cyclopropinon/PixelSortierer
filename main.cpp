a82f1688fbefae9dd544ff9586c4863f05e4ea06
Cyclopropinon <cyclopropinon@realprogrammers.org>
2025-01-07 00:14:24 +0100

    animiermöglichkeit hinzugefügt

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
+#include <cstdlib> // For std::atoi
+
 #include "pixelsortierer.cpp"
 
 int main(int argc, char const *argv[])
 {
-    return Pixelsortierer(argc, argv);
+    switch (std::atoi(argv[1])) // Sortiermethode
+    {
+    case 1:
+        return Pixelsortierer(argc, argv);
+        break;
+    case 2:
+        return PixelsortiererAnimiert(argc, argv);
+        break;
+    
+    default:
+        break;
+    }
 }
--- pixelsortierer.cpp
+++ pixelsortierer.cpp
@@ -64,7 +64,7 @@ int Pixelsortierer(int argc, char const *argv[])
 
         // Read an image
 //        auto image = readImage("input.png", width, height, channels);
-        auto image = readImage(argv[1], width, height, channels);
+        auto image = readImage(argv[2], width, height, channels);
         std::cout << "Image read successfully! Dimensions: " << width << "x" << height << " Channels: " << channels << std::endl;
         std::cout << "SSE aktiviert: " << stbi__sse2_available() << std::endl;
 
@@ -97,6 +97,60 @@ int Pixelsortierer(int argc, char const *argv[])
         // Write the modified image
         writeImage("output.png", image, width, height, channels);
         std::cout << "\nImage written successfully to output.png!" << std::endl;
+
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
+
+int PixelsortiererAnimiert(int argc, char const *argv[])
+{
+    try
+    {
+        int width, height, channels, Sortiermethode, diff;
+
+        // Read an image
+//        auto image = readImage("input.png", width, height, channels);
+        auto image = readImage(argv[2], width, height, channels);
+        std::cout << "Image read successfully! Dimensions: " << width << "x" << height << " Channels: " << channels << std::endl;
+        std::cout << "SSE aktiviert: " << stbi__sse2_available() << std::endl;
+
+        /*/ Modify the image (optional): Example - invert colors
+        for (auto& row : image)
+        {
+            for (auto& pixel : row)
+            {
+                for (auto& channel : pixel)
+                {
+                    channel = 255 - channel; // Simple inversion
+                }
+            }
+        }// */
+
+        std::cout << "Sortiermethode: ";
+        std::cin >> Sortiermethode;
+
+        //std::cout << "Sortierstärke (0 - 255): ";
+        //std::cin >> diff;
+
+        std::cout << "Sortieren ." << std::flush;
+
+        for (diff = 0; diff < 256; diff++)
+        {
+            for (auto& row : image)
+            {
+                OddEvenSort(row, Sortiermethode, diff);
+            }
+            writeImage(std::string("outanim_") + std::to_string(diff) + ".png", image, width, height, channels);
+            std::cout << '.' << std::flush;
+        }
+        
+        // Write the modified image
+        //writeImage("output.png", image, width, height, channels);
+        std::cout << "\nImage written successfully to outanim_[0-255].png!" << std::endl;
 
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
